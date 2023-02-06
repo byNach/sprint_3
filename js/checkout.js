c6f269aa48a3f434e3296213b6c4c712d4ed6d37
nach <nachnitror@gmail.com>
2023-02-06 10:52:08 +0100

    RM bootstrap validation ADD JS validation

--- js/checkout.js
+++ js/checkout.js
@@ -1,63 +1,69 @@
-
-// Exercise 6
-function validate() {
+function validate(event) {
 	var error = 0;
-	// Get the input fields
-	var fName = document.getElementById("fName").value;
-	var fLastN = document.getElementById("fLastN").value;
-	var fEmail = document.getElementById("fEmail").value;
-	var fPassword = document.getElementById("fPassword").value;
-	var fAddress = document.getElementById("fAddress").value;
-	var fPhone = document.getElementById("fPhone").value;
+	event.preventDefault();
 
-	const onlyLetters = (/^([a-zA-ZáéíóúüÁÉÍÓÚÜñÑ]{3,20}[\,\-\.]{0,1}[\s]{0,1}){1,3}$/);
-	const mailFormat = (/^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/);
-	const passFormat = (/^(?=.*\d)(?=.*[a-zA-Z]).{4,8}$/);
+	var fName = document.getElementById("fName");
+	var fEmail = document.getElementById("fEmail");
+	var fAddress = document.getElementById("fAddress");
+	var fLastN = document.getElementById("fLastN");
+	var fPassword = document.getElementById("fPassword");
+	var fPhone = document.getElementById("fPhone");
 
-	if (onlyLetters.test(fName) !== true) {
+	const onlyLettersRegEx = (/^([a-zA-ZáéíóúüÁÉÍÓÚÜñÑ]{3,20}[\,\-\.]{0,1}[\s]{0,1}){1,3}$/);
+	const emailRegEx = (/^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/);
+	const passwordRegEx = (/^(?=.*\d)(?=.*[a-zA-Z]).{4,8}$/);
+
+	//validate Name
+	if (fName.value.length < 3 || onlyLettersRegEx.test(fName.value) == false) {
+		fName.classList.add("is-invalid");
 		error++;
-		console.log(errorName);
-	}
-	if (onlyLetters.test(fLastN) !== true) {
-		error++;
-		console.log(errorLastN);
-	}
-	if (mailFormat.test(fEmail) !== true) {
-		error++;
-		console.log(errorEmail);
-	}
-	if (passFormat.test(fPassword) !== true) {
-		error++;
-		console.log(errorPassword);
-	}
-	if (fAddress.length < 3) {
-		error++;
-		console.log(errorAddress);
-	}
-	if (fPhone.length != 9) {
-		error++;
-		console.log(errorPhone);
-	}
-	if (error > 0) {
-		alert("Error");
 	} else {
-		alert("OK");
+		fName.classList.remove("is-invalid");
+		fName.classList.add("is-valid");
+	}
+
+	//validate Last Name
+	if (fLastN.value.length < 3 || onlyLettersRegEx.test(fLastN.value) == false) {
+		fLastN.classList.add("is-invalid");
+		error++;
+	} else {
+		fLastN.classList.remove("is-invalid");
+		fLastN.classList.add("is-valid");
+	}
+
+	//validate eMail
+	if (fEmail.value.length < 3 && emailRegEx.test(fEmail.value) == false) {
+		fEmail.classList.add('is-invalid');
+		error++;
+	} else {
+		fEmail.classList.remove("is-invalid");
+		fEmail.classList.add("is-valid");
+	}
+
+	//validate password
+	if (passwordRegEx.test(fPassword.value) == false) {
+		fPassword.classList.add('is-invalid');
+		error++;
+	} else {
+		fPassword.classList.remove("is-invalid");
+		fPassword.classList.add("is-valid");
+	}
+
+	//validate Address
+	if (fAddress.value.length < 3) {
+		fAddress.classList.add('is-invalid');
+		error++;
+	} else {
+		fAddress.classList.remove("is-invalid");
+		fAddress.classList.add("is-valid");
+	}
+
+	//validate phone number
+	if (fPhone.value.length < 9) {
+		fPhone.classList.add('is-invalid');
+		error++;
+	} else {
+		fPhone.classList.remove("is-invalid");
+		fPhone.classList.add("is-valid");
 	}
 }
-(function() {
-	'use strict';
-	window.addEventListener('load', function() {
-	  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-	  var forms = document.getElementsByClassName('needs-validation');
-	  // Loop over them and prevent submission
-	  var validation = Array.prototype.filter.call(forms, function(form) {
-		form.addEventListener('submit', function(event) {
-		  if (form.checkValidity() === false) {
-			event.preventDefault();
-			event.stopPropagation();
-		  }
-		  form.classList.add('was-validated');
-		}, false);
-	  });
-	}, false);
-  })();
